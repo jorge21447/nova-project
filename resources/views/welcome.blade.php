c7d94ae11d39dba7aad46e58f24d5c483ee55038
Enrique <eluisdev@gmail.com>
2025-03-30 23:12:38 -0400

    cambios

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
 
-</body>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite('resources/css/app.css')
+    </head>
+    <body class="">
+        <header class="">
+           <h1 class="">hola mundo</h1>
+           <h2 class="">hola mundo 2</h2>
+        </header>
+    </body>
 </html>
